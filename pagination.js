4d8e4b5e28f0df57046a5fbfd279edfd3e910f85
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-02 13:19:07 +0600

    Created pagination links

--- pagination.js
+++ pagination.js
@@ -4,6 +4,18 @@ Vue.component('pagination', {
     current: {
       type: Number,
       default: 1,
+    },
+    total: {
+      type: Number,
+      default: 9
+    },
+    perPage: {
+      type: Number,
+      default: 9,
+    },
+    pageRange: {
+      type: Number,
+      default: 2
     }
   },
   // automaticly calculate some
@@ -14,13 +26,49 @@ Vue.component('pagination', {
     prevPage: function () {
       return this.current - 1;
     },
+    totalPages: function () {
+      return Math.ceil(this.total / this.perPage);
+    },
+    rangeStart: function () {
+      var start = this.current - this.pageRange;
+
+      if (start > 0) {
+        return start;
+      } else {
+        return 1;
+      }
+    },
+    rangeEnd: function () {
+      var end = this.current + this.pageRange;
+
+      console.log(end)
+      return (end < this.totalPages) ? end : this.totalPages;
+    },
+    pages: function () {
+      var pages = [];
+
+      for (var i = this.rangeStart; i < this.rangeEnd; i++) {
+        pages.push(i)
+      }
+
+      return pages;
+    }
   },
   methods: {
     /**
      * Отлавливаем событе изменения страницы
      */
-    changePage: function(page) {
+    changePage: function (page) {
       this.$emit('page-changed', page);
+    },
+    /**
+     * Может возникнуть ситуация, что пользователь будет листать на страницы со знаком - или больше чем есть, а грузить собственно нечего
+     */
+    hasPrev: function () {
+      return this.current > 1;
+    },
+    hasNext: function () {
+      return this.current < this.totalPages
     }
   }
 })
