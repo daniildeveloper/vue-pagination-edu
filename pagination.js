0e83411abaf0f85a9fcb474f5c5f46a677878b72
Daniil Borovkov <daniilborowkow@yandex.ru>
2018-01-02 12:27:39 +0600

    Created base pagination

--- pagination.js
+++ pagination.js
@@ -1,3 +1,26 @@
 Vue.component('pagination', {
   template: '#pagination-template',
+  props: {
+    current: {
+      type: Number,
+      default: 1,
+    }
+  },
+  // automaticly calculate some
+  computed: {
+    nextPage: function () {
+      return this.current + 1;
+    },
+    prevPage: function () {
+      return this.current - 1;
+    },
+  },
+  methods: {
+    /**
+     * Отлавливаем событе изменения страницы
+     */
+    changePage: function(page) {
+      this.$emit('page-changed', page);
+    }
+  }
 })
